6f04428f50c99d9e4ef75a17c7b23a5698e37b68
Jonas Tratz <jonastratz@yahoo.de>
2019-10-14 20:39:17 +0200

    added structure

--- checker.sh
+++ checker.sh
@@ -1,9 +1,9 @@
 echo "--- Car: starting Checkstyle ---"
-java -jar checkstyle-8.25-all.jar -c checkstyle.xml car/Car.java
+java -jar libs/checkstyle-8.25-all.jar -c checkers_xml/checkstyle.xml src/car/Car.java
 echo "--- Car:  starting PMD ---"
-pmd-bin-6.18.0/bin/run.sh pmd  -no-cache -d car/Car.java -f text  -R pmd.xml
+libs/pmd-bin-6.18.0/bin/run.sh pmd  -no-cache -d src/car/Car.java -f text  -R checkers_xml/pmd.xml
 echo "____________________________________________________________________"
 echo "--- LicenceAdministration: starting Checkstyle ---"
-java -jar checkstyle-8.25-all.jar -c checkstyle.xml car/LicenceAdministration.java
+java -jar libs/checkstyle-8.25-all.jar -c checkers_xml/checkstyle.xml src/car/LicenceAdministration.java
 echo "--- LicenceAdministration:  starting PMD ---"
-pmd-bin-6.18.0/bin/run.sh pmd  -no-cache -d car/LicenceAdministration.java -f text  -R pmd.xml
+libs/pmd-bin-6.18.0/bin/run.sh pmd  -no-cache -d src/car/LicenceAdministration.java -f text  -R checkers_xml/pmd.xml
